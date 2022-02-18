a5593f9b112e68e61a7c3666ddbe9329152d2201
raulalvesr <raul.alves.re@hotmail.com>
2022-02-18 18:46:42 -0300

    sub

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    private static void main(String[] args) {
+        System.out.println(soma(1, 1));
+    }
+
+    private static int soma(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    private static int sub(int n1, int n2) {
+        return n1 - n2;
+    }
+    
+}
